e447a6f4dc808b1aed8af1bc05cb1ec2b286c16b
Jawad Bin Jahangir <jawadjb.21@gmail.com>
2026-02-23 11:29:41 +0100

    Changed html redundancies and added dynamic counter

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
-document.addEventListener("DOMContentLoaded", function(){
-    
-})
+let totalCount = document.getElementById("total-counter");
+let acceptCounter = document.getElementById("accept-counter");
+let rejectCount = document.getElementById("reject-counter");
+
+// Returns an HTML collection of the children, on which we apply length method.
+let noOfCards = document.getElementById("job-cards").children.length;
+console.log(noOfCards);
+
+function countJobs(){
+    totalCount.innerText = noOfCards;
+}
+
+countJobs();
